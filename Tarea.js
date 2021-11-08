affda812946f1df9dee6cf5a79c5f060c1545d5d
Serginator99 <92330120+Serginator99@users.noreply.github.com>
2021-11-08 16:46:15 -0500

    Add files via upload

---
+++ Tarea.js
@@ -0,0 +1,11 @@
+var n1 , n2 , suma , resta , multiplicacion , division
+n1= prompt("Primer Numero") ;
+n2= prompt("Segundo Numero") ;
+suma = (parseInt(n1)+ parseInt(n2)) ;
+resta = (parseInt(n1)- parseInt(n2)) ;
+multiplicacion = (parseInt(n1)* parseInt(n2)) ;
+division = (parseInt(n1)/ parseInt(n2)) ;
+console.log (`Hola, la suma es ${suma}`)
+console.log (`Hola, la resta es ${resta}`)
+console.log (`Hola, la multiplicacion es ${multiplicacion}`)
+console.log (`Hola, la division es ${division}`)
